985dbe0bbafa6ad4c72d4c5e972001159aa1246d
Nitin <harehn.kaundun@mail.mcgill.ca>
2025-11-14 23:43:43 -0500

    Cleared code

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const Wrapper = (props) => {
 
 const Footer = () => {
   return (
-    <footer className='footer-container'>
+    <footer>
       <div className='footer'>
         <div class="links">
           <a href="#">About Website</a>
@@ -64,36 +64,5 @@ const App = () => {
     </div>
   )
 }
-
-// style="border-width:0" width="500" height="400" frameborder="0" scrolling="no"
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="Wrapper">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
 
 export default App
