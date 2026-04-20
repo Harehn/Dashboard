148177167f4a08fa72dd933b32c8a5944bb5f7a5
Nitin <harehn.kaundun@mail.mcgill.ca>
2026-04-20 18:54:12 -0400

    Trying out certain functions in API calls

--- src/Components/APICalls.jsx
+++ src/Components/APICalls.jsx
@@ -1,14 +1,49 @@
 export default async function fetchRSSQuote() {
   try{
+    // let fetchUrl = 'https://api.chess.com/pub/player/overthinkingbell/stats';
+    // const response = await fetch(fetchUrl);
+    // const data = await response.text();
+    // return data;
+
+    //--------API CALLS-----------
+    // console.log('Fetching URL:', fetchUrl); // Debugging 1: Log the request URL
+    // const response = await fetch(fetchUrl);
+    // const data = await response.text();
+    
+    // // console.log('Data fetched:', data); // Debugging 2: Log the raw data
+    
+    // const parser = new DOMParser();
+    // const xmlDoc = parser.parseFromString(data, "application/xml");
+    // const item = xmlDoc.getElementsByTagName("item")[0];
+    // const str = item.querySelector("description").textContent;
+    // return str;    
+    //----------------------------
   return "Placeholder for Quote";    
   }catch (error) {
     console.log('Error fetching the RSS feed:', error);
   }  
   return "ERROR";
 }
+Array.prototype.random = function () {
+  return this[Math.floor((Math.random()*this.length))];
+}
+
+export async function getRandomQuote(quotes){
+  try {
+    console.log(quotes.random());
+    let newQuote = quotes.random();
+    return String(newQuote['quote']) + ' - ' + String(newQuote['author']);
+    return [12,13,14].random();
+  } catch {
+    return "ERROR";
+  }
+}
 
 export async function fetchRSSWord() {
   try {
+    const envvar = import.meta.env.VITE_APP_API_URL;
+    console.log(envvar);
+    return envvar;
     return "Placeholder for Word of the day"
   }catch (error) {
     console.log('Error fetching the RSS feed:', error);
