827770e379d811dbc9dc5718db1b550c539feebb
Nitin <harehn.kaundun@mail.mcgill.ca>
2026-04-22 18:11:53 -0400

    Added buttons for adding and removing tasks

--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -1,6 +1,14 @@
 export default function Task() {
+  try{
+  people = ['Alice', 'Bob', 'Charlie'];
+  const listItems = people.map(person => <li>{person}</li>);
+  }catch {
+
+  }
   return (
     <div id='taskcontainer'>
+      <div id='tasks'>
+        {/* {listItems} */}
         <input type="checkbox" id="task1" name="task1" value=""/>
         <label htmlFor="task1">Read emails</label> <br/>
         <input type="checkbox" id="task2" name="task2" value=""/>
@@ -9,5 +17,16 @@ export default function Task() {
         <label htmlFor="task3"> Exercise</label> <br/>
         <input type="checkbox" id="task4" name="task4" value=""/>
         <label htmlFor="task4">Journal</label> <br/>
-    </div>);
+      </div>
+      <div id="empty"></div>
+      <div className='buttons'>
+        <button className='clear'>Clear checked tasks</button>
+      </div>
+      <div id='adder'>
+        <input type="text" id='enter' placeholder="Eg. Buy Groceries"/>
+        <button id="add">Add New</button>
+      </div>
+    </div>
+
+    );
 }
