4b8902fe661cfcb692e4512660c50dfa49872d76
Nitin <harehn.kaundun@mail.mcgill.ca>
2026-04-27 19:04:22 -0400

    Added an axios api all to the news

--- src/Components/APICalls.jsx
+++ src/Components/APICalls.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 export default async function fetchRSSQuote() {
   try{
     // let fetchUrl = 'https://api.chess.com/pub/player/overthinkingbell/stats';
@@ -41,43 +42,49 @@ export async function getRandomQuote(quotes){
 
 export async function fetchRSSWord() {
   try {
-    const envvar = import.meta.env.VITE_APP_API_URL;
-    console.log(envvar);
-    return envvar;
+    // const envvar = import.meta.env.VITE_APP_API_URL;
+    // console.log(envvar);
+    // return envvar;
     return "Placeholder for Word of the day"
   }catch (error) {
     console.log('Error fetching the RSS feed:', error);
   }  
   return "ERROR";
 }
-
-export async function fetchNews() {
-  return "Could not connect to AP news at the moment. Please try later.";
-  const proxyUrl = 'https://cors-anywhere.com/';
-  const targetUrl = 'https://feedx.net/rss/ap.xml';
-  const fetchUrl = `${proxyUrl}${encodeURIComponent(targetUrl)}`;
-  try {
-    console.log('Fetching URL:', fetchUrl); // Debugging 1: Log the request URL
-    const response = await fetch(fetchUrl);
-    const data = await response.text();
-      console.log(data); // Debugging 2: Log the raw data
-    
-    const parser = new DOMParser();
-    const xmlDoc = parser.parseFromString(data, "application/xml");
-    const item = xmlDoc.getElementsByTagName("item")[0];
-    const str = item.querySelector("description").textContent;
-    const innerdocs = parser.parseFromString(str, "text/html").querySelectorAll("p");
-    return innerdocs[1].innerText;
-    // let st = innerdocs[1].innerText.replaceAll(" ", "").split("•")
-    // st = st[0] + " ( " + st[2] +")";
-    // let st2 = innerdocs[2].innerText;
-    // console.log([st,st2]);
-    // return st + "19" + st2;
-  }catch (error) {
-    console.log('Error fetching the RSS feed:', error);
-  }  
-  return "ERROR";
+export async function fetchNews(){
+  const envvar = import.meta.env.VITE_APP_API_URL;
+  axios.get(envvar)
+    .then((response) => console.log(response.data));
+    return "Could not connect to AP news at the moment. Please try later.";
 }
+
+// export async function fetchNews() {
+//   return "Could not connect to AP news at the moment. Please try later.";
+//   const proxyUrl = 'https://cors-anywhere.com/';
+//   const targetUrl = 'https://feedx.net/rss/ap.xml';
+//   const fetchUrl = `${proxyUrl}${encodeURIComponent(targetUrl)}`;
+//   try {
+//     console.log('Fetching URL:', fetchUrl); // Debugging 1: Log the request URL
+//     const response = await fetch(fetchUrl);
+//     const data = await response.text();
+//       console.log(data); // Debugging 2: Log the raw data
+    
+//     const parser = new DOMParser();
+//     const xmlDoc = parser.parseFromString(data, "application/xml");
+//     const item = xmlDoc.getElementsByTagName("item")[0];
+//     const str = item.querySelector("description").textContent;
+//     const innerdocs = parser.parseFromString(str, "text/html").querySelectorAll("p");
+//     return innerdocs[1].innerText;
+//     // let st = innerdocs[1].innerText.replaceAll(" ", "").split("•")
+//     // st = st[0] + " ( " + st[2] +")";
+//     // let st2 = innerdocs[2].innerText;
+//     // console.log([st,st2]);
+//     // return st + "19" + st2;
+//   }catch (error) {
+//     console.log('Error fetching the RSS feed:', error);
+//   }  
+//   return "ERROR";
+// }
 
 export async function fetchRSSGerman() {
   try {
